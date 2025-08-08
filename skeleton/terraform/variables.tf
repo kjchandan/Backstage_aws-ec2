3e1ca7ebb36a9fdd43d1d93ae745173a02a70242
CHANDAN K J <105358201+kjchandan@users.noreply.github.com>
2025-08-08 13:21:50 +0530

    Update variables.tf

--- skeleton/terraform/variables.tf
+++ skeleton/terraform/variables.tf
@@ -1,35 +1,23 @@
-variable "instance_name" {
-  description = "Name for the EC2 instance"
-  type        = string
-  default     = "my-ec2-instance"
-}
-
-variable "instance_description" {
-  description = "Description for the EC2 instance"
-  type        = string
-  default     = "Simple EC2 instance created by Backstage"
-}
-
 variable "aws_region" {
-  description = "AWS region where resources will be created"
+  description = "AWS region to launch the instance"
   type        = string
   default     = "us-east-1"
 }
-
+ 
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-08a6efd148b1f7504"  # Amazon Linux 2 AMI (for example)
+}
+ 
 variable "instance_type" {
-  description = "EC2 instance type - fixed to t2.micro for simplicity"
+  description = "Type of EC2 instance"
   type        = string
   default     = "t2.micro"
 }
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance - Amazon Linux 2023"
+ 
+variable "instance_name" {
+  description = "GOLDENPATH"
   type        = string
-  default     = "ami-0c02fb55956c7d316"
-}
-
-variable "key_pair_name" {
-  description = "Name of the AWS key pair for SSH access"
-  type        = string
-  default     = ""
+  default     = "GoldenPath-EC2"
 }
