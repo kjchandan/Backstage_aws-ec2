b6fa59568dfc79e3ea1a3f8db803d1b8562243ee
CHANDAN K J <105358201+kjchandan@users.noreply.github.com>
2025-08-08 14:12:42 +0530

    Update main.tf

--- skeleton/terraform/main.tf
+++ skeleton/terraform/main.tf
@@ -72,9 +72,10 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
  
   tags = {
-    Name = "Terraform-EC2"
+    Name = "Backstage-EC2"
   }
 }
+
 
 
 
