e78b8a028b9662504d47454d3c7daf9406bb87b4
CHANDAN K J <105358201+kjchandan@users.noreply.github.com>
2025-08-08 13:31:00 +0530

    Update main.tf

--- skeleton/terraform/main.tf
+++ skeleton/terraform/main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "ec2_sg" {
  
 # EC2 Instance
 resource "aws_instance" "example" {
-  ami                    = var.ami_id
+  ami                    = ami-084a7d336e816906b
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
@@ -72,3 +72,4 @@ resource "aws_instance" "example" {
     Name = "Terraform-EC2"
   }
 }
+
